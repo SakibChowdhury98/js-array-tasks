471366e0dfb95fb14d2f6808d56d67495c0208e4
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-08-31 10:48:45 +0600

    task 5

---
+++ task5.js
@@ -0,0 +1,5 @@
+// 5. Combining Arrays
+var fruits1 = ["apple", "banana", "coconut", "date", "mango"];
+var fruits2 = ["batabi lebu", "dragon", "malta"];
+fruits = fruits1.concat(fruits2);
+console.log(fruits);
