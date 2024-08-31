4475edeb47fb46f4a65e0370919b7f2f375f7e1f
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-08-31 10:45:41 +0600

    task 4

---
+++ task4.js
@@ -0,0 +1,5 @@
+// 4. Checking if it's an Array
+var fruits = ["apple", "banana", "coconut", "date", "mango"];
+var age = (12, 14, 34, 67);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(age));
