74b61ce7e5c31fca545a3e25fa20fd60c2926ef7
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-08-31 10:38:44 +0600

    task 2

--- task1.js
+++ task1.js
@@ -1,5 +1,5 @@
 // 1. Declare an array 
-var name = ["apple", "banana", "coconut", "date", "mango"]
+var name = ["apple", "banana", "coconut", "date", "mango"];
 console.log(name);
 console.log(name[3]);
 name[2] = "jambura";
---
+++ task2.js
@@ -0,0 +1,7 @@
+// 2. Add or remove elements
+var spot = ["coxsbazar", "rangamati", "sundarban"];
+spot.push("bandarban");
+spot.push("saint-martin", "kutubdia");
+spot.pop();
+console.log(spot);
+
